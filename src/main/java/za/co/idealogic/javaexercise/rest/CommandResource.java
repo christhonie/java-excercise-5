25c4b3961f55f1259d77e250b9383457e8ff4654
Christhonie Geldenhuys <development@christhonie.co.za>
2020-08-07 17:20:12 +0200

    Add add POST method

--- src/main/java/za/co/idealogic/javaexercise/rest/CommandResource.java
+++ src/main/java/za/co/idealogic/javaexercise/rest/CommandResource.java
@@ -1,10 +1,13 @@
 package za.co.idealogic.javaexercise.rest;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +33,10 @@ public class CommandResource {
     public ResponseEntity<List<String>> findAll() {
         return ResponseEntity.ok(persistenceService.getMessagesTheFancyWay());
     }
- 
+    
+    @PostMapping(path = "add/{message}")
+    public ResponseEntity<Void> addMessage(@PathVariable String message) {
+    	persistenceService.addMessage(message, Instant.now());
+    	return ResponseEntity.noContent().build();
+    }
 }
